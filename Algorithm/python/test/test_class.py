2acea5c56e06be381ed6b3a5200228df7d050911
xinmu123 <861238515@qq.com>
2022-09-06 18:01:11 +0800

    some

---
+++ Algorithm/python/test/test_class.py
@@ -0,0 +1,20 @@
+class A():
+    def __init__(self) -> None:
+        self.a = 1
+    
+    def fn_1(self):
+        ...
+    
+    @classmethod
+    def fn_2(cls):
+        ...
+    
+    @staticmethod
+    def fn_3():
+        ...
+    
+    def fn_4():
+        ...
+        
+a = A()
+
